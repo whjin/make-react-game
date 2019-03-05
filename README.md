f94848370c1cea67525034d9626efa641a4bade4
whjin <1414848321@qq.com>
2019-03-05 11:22:03 +0800

    :memo:部署到github pages

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # make-react-game
 :tada::rocket:React设计的一款棋盘小游戏
+
+# 将React项目部署到Github Pages #
+
+第一步，先把主文件`push`到`mater`分支。
+
+第二步，在`package.json`上加上`homepage`项，内容为项目地址。依赖需要安装`react-scripts`、`gh-pages`，并在`scripts`项中加入构建命令`deploy`。
+
+![](https://i.imgur.com/2I3mrTX.jpg)
+
+![](https://i.imgur.com/fMJlVke.jpg)
+
+![](https://i.imgur.com/89AOZ6j.jpg)
+
+第三步，分别执行`npm run build`和`npm run deploy`即可。
+
+
---
+++ build/precache-manifest.d2b4f82866b128ed92ebd0dc0c6886e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bbea2afef6f1dcc01d61",
+    "url": "/make-react-game/static/js/runtime~main.bbea2afe.js"
+  },
+  {
+    "revision": "3904b812176ed81da283",
+    "url": "/make-react-game/static/js/main.3904b812.chunk.js"
+  },
+  {
+    "revision": "52897388c9d087d6c14f",
+    "url": "/make-react-game/static/js/2.52897388.chunk.js"
+  },
+  {
+    "revision": "3904b812176ed81da283",
+    "url": "/make-react-game/static/css/main.792a3cd7.chunk.css"
+  },
+  {
+    "revision": "eeadbf1e65c101c456667a2b0106e919",
+    "url": "/make-react-game/index.html"
+  }
+];
